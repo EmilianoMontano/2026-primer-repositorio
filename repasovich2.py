b5058c4228083267e520a2a66f9205c318173c0d
EmilianoMontano <e.montano@alumno.etec.um.edu.ar>
2026-03-30 08:58:59 -0300

    Repasovich2bradar

--- repasovich2.py
+++ repasovich2.py
@@ -1,8 +1,8 @@
 print("Bradar pls create a logo for my company called Detal Mick")
-a=input=("Create?\n")
-if a="yes":
-    print("You created the logo with neon light, futuristic city and skyscraper")
-elif a="no":
+a=input("Create?\n")
+if a=="yes":
+    print("You created the logo with neon lights, futuristic city and skyscraper")
+elif a=="no":
     print("Bradar what is this? Delete this comment")
 else:
     print("Bradar what did you write? Retry bradar")
